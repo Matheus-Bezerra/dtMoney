a1257284864787fe2fb922dd26e8460d7c3a86c9
Matheus Bezerra <matheus.bezerra@j4s.tech>
2022-09-05 23:59:20 -0300

    Componente Summary estilizado

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SummaryCard, SummaryContainer } from './styles';
+import { ArrowCircleUp, ArrowCircleDown } from 'phosphor-react';
+
+export const Summary = () => {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <ArrowCircleDown size={32} color="#fff" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  );
+};
