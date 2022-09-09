984093967b8d6bf7a85db5c486e5e32cb7f3f7af
Matheus Bezerra <matheus.bezerra@j4s.tech>
2022-09-09 00:12:42 -0300

    Estrutura visual complemetamente feita

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import LogoImg from '../../assets/logo.svg';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={LogoImg} alt="Logo Dt Money" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { SearchForm } from './components/SearchForm';
 import {
   PriceHighLight,
   TransactionsContainer,
@@ -14,6 +15,7 @@ export const Transactions = () => {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
